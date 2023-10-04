c000b90d82a349e30abdba155ad9dd18db80aa92
Rae Stanton <raestanton93@gmail.com>
2023-10-04 12:31:25 -0700

    fin get API

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 from models import db, User, Review, Game
 
 app = Flask(__name__)
@@ -15,13 +16,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
 
+
+@app.route("/games")
+def games():
+
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(
+        games,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 # start building your API here
+
+
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    game_dict = game.to_dict()
+
+    response = make_response(
+        game_dict,
+        200
+    )
+
+    return response
+
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    # use association proxy to get users for a game
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    response = make_response(
+        users,
+        200
+    )
+
+    return response
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
